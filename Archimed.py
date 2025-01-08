8c6d66a3bbd3506659c6eed82fbbfb3923688f1b
Kuga09 <glal64@tutamail.com>
2025-01-08 12:57:06 +0300

    second commit

--- Archimed.py
+++ Archimed.py
@@ -5,10 +5,12 @@
 def archimed(m,v):
     f_mg = float(m)*9.81
     f_arch = float(v)*9.81
-    if f_arch >= f_mg:
-        return True
+    if f_arch > f_mg:
+        return 1
+    elif f_arch < f_mg:
+        return 0
     else:
-        False
+        return 2
 
 # Функция main
 def main():
@@ -16,10 +18,12 @@ def main():
     inpt=input().split()
     m = int(inpt[0])
     v = int(inpt[1])
-    if archimed(m,v) == True:
+    if archimed(m,v) == 1:
         print('Объект плавучий')
+    elif archimed(m,v) == 0:
+        print('Объект не обладает плавучестью')
     else:
-        print('Объект не обладает плавучастью')
+        print('Объект обладает частичной плавучестью')
 
 
 if __name__ == '__main__':
